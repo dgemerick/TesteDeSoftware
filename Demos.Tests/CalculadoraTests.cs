a98671e8c2fa174dd92f828bcf2e942a4971e021
Douglas Emerick <douglas@emerick.net.br>
2021-09-11 15:55:24 -0300

    Add project files.

---
+++ Demos.Tests/CalculadoraTests.cs
@@ -0,0 +1,25 @@
+﻿using Demo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demos.Tests
+{
+    public class CalculadoraTests
+    {
+
+        [Fact]
+        public void Calculadora_Somar_RetornaValorSoma()
+        {
+            //Arange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(2, 2);
+
+            //Assert
+            Assert.Equal(4, resultado);
+        }
+    }
+}
